319809e040a7b4c3f32ed24bc35f9c243e257214
Juan Vazquez <ehulises28@hotmail.com>
2021-11-12 11:15:55 -0800

    created first get route to get users thoughts

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
-const express = requiere("exprees");
-const app = exprees();
-const PORT = process.env.port || 3001;
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
 const userRoutes = require("./routes/user-routes");
 
-//express middleware, used to be bodyparser
+// express middleware, used to be bodyparser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Serve up Static assets
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// app.use(require('./routes));
+
+// app.use(require('./routes'));
 app.use("/api/", userRoutes);
+
 // Start the API server
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
